6dfddbfbb5aee9be50432e4c311ccbd5a6323547
mister-blanket <michael@mrplunkett.com>
2020-08-16 18:24:03 -0500

    Add moving shadow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import movingShadow from 'moving-shadow';
 
 import Filter from './Filter';
 import Sort from './Sort';
@@ -21,6 +22,33 @@ const App = ({ rawData }) => {
 
   // Filter data
   // useEffect(() => { setData(handleFilter(data, filterType, filterInput)) }, [filterType, filterInput]);
+
+  useEffect(() => {
+    const fixedColor = "#665200"
+    const shadowProp = {
+      shadowType:   "shadow", // "shadow", "dropShadow"
+      selector:     ".title", // tag, class, or id to apply shadow to
+      angle:        20, // height of view source. Should be between 10 - 100
+      diffusion:    0, // blur-radius
+      color:        "#997a00", // shadow-color
+      xOffset:      -7, // X offset value, set to max fixedShadow x offset
+      yOffset:      12, // Y offset value, set to max fixedShadow y offset
+      fixedShadow:  `
+        -1px 1px 0px ${fixedColor},
+        -1px 2px 0px ${fixedColor},
+        -2px 3px 0px ${fixedColor},
+        -2px 4px 0px ${fixedColor},
+        -3px 5px 0px ${fixedColor},
+        -3px 6px 0px ${fixedColor},
+        -4px 7px 0px ${fixedColor},
+        -4px 8px 0px ${fixedColor},
+        -5px 9px 0px ${fixedColor},
+        -5px 10px 0px ${fixedColor},
+        -6px 11px 0px ${fixedColor},
+        -7px 12px 0px ${fixedColor}`,  // "5px 5px #555" to include an optional fixed shadow
+    }
+    movingShadow(shadowProp);
+  }, [])
 
   // Sort data
   // Run filter & sort
