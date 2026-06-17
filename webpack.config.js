31740cb23391fc091d27ab5f261d82b7d8077924
Kay <kayla.lee.plunkett@gmail.com>
2026-06-17 13:17:49 -0500

    Update favicon color

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,65 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Vinyl',
+      title: "Vinyl",
       template: "./src/index.html",
-      favicon: "./src/favicon.png"
-    })
+      favicon: "./src/favicon_black.png",
+    }),
   ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
     proxy: {
-      '/api/itunes': {
-        target: 'https://itunes.apple.com',
+      "/api/itunes": {
+        target: "https://itunes.apple.com",
         changeOrigin: true,
-        pathRewrite: { '^/api/itunes': '/search' },
+        pathRewrite: { "^/api/itunes": "/search" },
         onProxyReq: (proxyReq) => {
           // Strip all browser headers that Apple may reject, then set a clean User-Agent
-          const keep = ['host', 'accept-encoding'];
+          const keep = ["host", "accept-encoding"];
           for (const name of proxyReq.getHeaderNames()) {
             if (!keep.includes(name)) proxyReq.removeHeader(name);
           }
-          proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (compatible; VinylApp/1.0)');
+          proxyReq.setHeader(
+            "User-Agent",
+            "Mozilla/5.0 (compatible; VinylApp/1.0)",
+          );
         },
       },
     },
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader',
+          "sass-loader",
         ],
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               esModule: false,
             },
@@ -68,25 +68,23 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader',
-        ],
+        use: ["file-loader"],
       },
       {
         test: /\.csv$/,
-        loader: 'csv-loader',
+        loader: "csv-loader",
         options: {
           dynamicTyping: true,
           header: true,
-          skipEmptyLines: true
-        }
+          skipEmptyLines: true,
+        },
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
     ],
   },
